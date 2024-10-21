eaf8f0b0d5c854976ea71ce146adba15cf886dbd
Giovani Marcarini <inf_giovani@hotmail.com>
2024-10-21 17:28:29 +0100

    IoT com Flask, inicialização

---
+++ app/controllers/default.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as gpio 
+import time as delay
+from app import app
+from flask import render_template
+
+gpio.setmode(gpio.BOARD)
+gpio.setwarnings(False)
+
+ledVermelho, ledVerde = 11, 12
+
+statusVermelho = ""
+statusVerde = ""
+
+gpio.setup(ledVermelho, gpio.OUT)
+gpio.setup(ledVerde, gpio.OUT)
+
+gpio.output(ledVermelho, gpio.LOW)
+gpio.output(ledVerde, gpio.LOW)
+
+def status_led_vermelho():
+    if gpio.input(ledVermelho) == 1:
+        statusVermelho = 'LED vermelho ON'
+    else:
+        statusVermelho = 'LED vermelho OFF'
+
+    return statusVermelho
+
+def status_led_verde():
+    if gpio.input(ledVerde) == 1:
+        statusVerde = 'LED verde ON'
+    else:
+        statusVerde = 'LED verde OFF'
+
+    return statusVerde
+
+@app.route("/")
+def index():
+    templateData = {
+        'ledRed': status_led_vermelho(),
+        'ledGreen': status_led_verde()
+    }
+    return render_template('index.html', **templateData)
